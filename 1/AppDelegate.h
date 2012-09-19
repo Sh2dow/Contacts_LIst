6111b3aa088a36204d99bd6dd627cf10f43ccdc5
Sh2dow <sh2dow@Mac-Pro-Sh2dow.local>
2012-09-19 03:10:14 +0300

    Initial Commit

---
+++ 1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1
+//
+//  Created by Sh2dow on 19.09.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UINavigationController *navigationController;
+
+@end
---
+++ 1/MasterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewController.h
+//  1
+//
+//  Created by Sh2dow on 19.09.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@end
